8cd40608cccbf358b37e57464b7a3f238bb1a53c
Rabins-Khanal <sjlkhanal@gmail.com>
2025-03-01 19:53:00 +0545

    initialized

---
+++ index.js
@@ -0,0 +1,55 @@
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+import authRoutes from './routes/auth.route.js';
+import commentRoutes from './routes/comment.route.js';
+import fixtureRoutes from './routes/fixture.route.js';
+import postRoutes from './routes/post.route.js';
+import userRoutes from './routes/user.route.js';
+
+dotenv.config({ path: './config.env' });
+
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
+app.use('/api/fixture', fixtureRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
